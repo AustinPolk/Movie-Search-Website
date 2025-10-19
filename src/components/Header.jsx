040684132ce205057e4c4dcef2fb09a97aa167bd
Austin Polk <austinpolk32@gmail.com>
2025-10-19 15:25:44 -0500

    Update nav links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,9 +7,9 @@ function Header()
             <h2>Movie Search</h2>
             <h3>In Search of Good Movies</h3>
             <nav>
-                <Link to="/" >Home</Link>
-                <Link to="/search" >Search</Link>
-                <Link to="/watchlist" >Watchlist</Link>
+                <Link to="/Movie-Search-Website" >Home</Link>
+                <Link to="/Movie-Search-Website/search" >Search</Link>
+                <Link to="/Movie-Search-Website/watchlist" >Watchlist</Link>
             </nav>
         </header>
     );
